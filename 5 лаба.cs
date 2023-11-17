eabbbd649e92ff048ae01d41e0c2af82cd03e2de
sabinka-collab <149395167+sabinka-collab@users.noreply.github.com>
2023-11-17 11:42:09 +0300

    Add files via upload

---
+++ 5 лаба.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+    int[] Value= new int[x];
+for (int p = 0; p < x; p++) {
+    p = Convert.ToInt32(Console.ReadLine());
+}
+//int i = 0;
+for (int k = 0; k < x; k++)
+{
+    if (Value[k] > s)
+    {
+        Value[k] = s;
+        s++;
+    }
+}
+Console.WriteLine(Value);
+Console.WriteLine(s);
